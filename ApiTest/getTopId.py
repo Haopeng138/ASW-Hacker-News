fd23e00f9227a5020a5034ea72e2568aeed53c0e
haopeng.lin <haopeng.lin@est.fib.upc.edu>
2022-10-21 01:48:03 +0200

    funciones para utlitzar las apis

--- ApiTest/getTopId.py
+++ ApiTest/getTopId.py
@@ -1,6 +1,9 @@
+from webbrowser import get
 import requests
 from urllib.parse import urlparse
 import json 
+import math
+import datetime
 
 def getTopIds():
   respon = requests.get(url="https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty")
@@ -19,27 +22,44 @@ def getDomain(url):
     domain = urlparse(url).netloc
     return str(domain)
 
+def diffTimeHour(tp):
+    dt = datetime.datetime.now()
+    t2d = datetime.datetime.fromtimestamp(tp)
+    diff = dt - t2d
+    hours = diff.seconds/3600
+    return math.trunc(hours) 
+
 def getTop30News():
     newslist = []
     idlist = getTop30()
 
     for id in idlist:
-        url = "https://hacker-news.firebaseio.com/v0/item/"+str(id)+".json?print=pretty"
-        re = requests.get(url=url)
-        noticia = re.json()
-        
-        tmp = {
-            'title':noticia['title'],
-            'url':noticia['url'],
-            'descendants':noticia['descendants'],
-            'time':
-        }
-        
-        # for key in noticia.keys():
-        #     print(key)
-        newslist.append(noticia)
+        try:
+            url = "https://hacker-news.firebaseio.com/v0/item/"+str(id)+".json?print=pretty"
+            re = requests.get(url=url)
+            noticia = re.json()
+            t = diffTimeHour(noticia["time"])
+            tmp = {
+                'title':noticia['title'],
+                'url':noticia['url'],
+                'descendants':noticia['descendants'],
+                'time': t,
+                'score':noticia['score'],
+                'by':noticia['by']
+            }
+            
+            
+            # for key in noticia.keys():
+            #     print(key)
+            newslist.append(tmp)
+        except:
+            pass
+
+    print(newslist)
 
     return 1
 
 
+
+getTop30News()
 ## Que necesito : title, url,descendants,time ,score,by 
--- ApiTest/test.py
+++ ApiTest/test.py
@@ -1,12 +1,13 @@
-from datetime import datetime
-import math
-dt = datetime.now()
-timestamp = 1666209109
-t2d = datetime.fromtimestamp(timestamp)
+# from datetime import datetime
+# import math
 
-diff = dt - t2d
-# getting the timestamp
-hours = diff.seconds/3600
+# def diffTimeHour(timestamp):
 
+#     dt = datetime.now()
+#     t2d = datetime.fromtimestamp(timestamp)
+#     diff = dt - t2d
+#     hours = diff.seconds/3600
+#     return math.trunc(hours) 
 
-print(math.trunc(hours))
+# timestamp = 1666209109
+
