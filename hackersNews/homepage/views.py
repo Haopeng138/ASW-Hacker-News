2735d3ffc15dea3ac3170fba8f52ccd7c6c1db93
Marc Duch <marcduchb20@gmail.com>
2022-10-20 17:14:41 +0200

    Añadido /login a homepage

--- hackersNews/homepage/views.py
+++ hackersNews/homepage/views.py
@@ -4,3 +4,8 @@ from django.template import loader
 def index(request):
   template = loader.get_template('homepage.html')
   return HttpResponse(template.render())
+
+
+def login(request):
+  template = loader.get_template('login.html')
+  return HttpResponse(template.render())
