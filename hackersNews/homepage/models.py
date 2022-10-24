a94df434f782b56702e6d0cb82ec3750846f5f8d
marc.duch <marc.duch@estudiantat.upc.edu>
2022-10-24 15:44:18 +0200

    Modelo para guardar Usuarios

--- hackersNews/homepage/models.py
+++ hackersNews/homepage/models.py
@@ -3,7 +3,16 @@ from django.db import models
 
 # Create your models here.
 
+# Test
 class Login(models.Model):
     username = models.CharField(max_length=255)
     username.primary_key=True
     password = models.CharField(max_length=255)
+
+class User(models.Model):
+    # id - Implicito
+    username = models.CharField(max_length=255, unique=True)
+    password = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, unique=True)
+    karma = models.Field(default=0)
+
