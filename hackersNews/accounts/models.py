b4496346b7e759aa0e410c2ae6583e864fb9a654
marc.duch <marc.duch@estudiantat.upc.edu>
2022-11-07 11:28:05 +0100

    Canvio de formato

--- hackersNews/accounts/models.py
+++ hackersNews/accounts/models.py
@@ -1,45 +1,46 @@
-from django.db import models 
+from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 import datetime
 
 
 # Create your models here.
 
+# User Manager
 class HackerNewsUserManager(BaseUserManager):
     def create_user(self, username, email, password=None):
         # Crea y guarda un usuario
-        
+
         if not email:
             raise ValueError('Email field cannot be empty')
 
         user = self.model(
-            username = username,
-            email = self.normalize_email(email),
-            date_joined = datetime.datetime.now() # timezone.now()
-            )
+            username=username,
+            email=self.normalize_email(email),
+            date_joined=datetime.datetime.now()  # timezone.now()
+        )
 
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-# Crea un usuario admin
+    # Crea un usuario admin
     def create_superuser(self, email, username, password=None):
         user = self.create_user(username, email=email, password=password)
         user.is_admin = True
         user.save(using=self._db)
         return user
 
+
 # Model de Usuario
 class HNUser(AbstractBaseUser):
-    
-    id          = models.AutoField(primary_key=True)
-    username    = models.CharField(max_length=255, unique=True)
-    email       = models.EmailField(
-                        verbose_name ='Email addres',
-                        max_length=255,
-                        unique=True,
-                )
-    karma       = models.Field(default=0)
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(
+        verbose_name='Email address',
+        max_length=255,
+        unique=True,
+    )
+    karma = models.Field(default=0)
 
     date_joined = models.DateField()
 
@@ -47,27 +48,23 @@ class HNUser(AbstractBaseUser):
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
 
-
-    USERNAME_FIELD  = 'email'       # Especifica que identidfica un usuario
-    EMAIL_FIELD     = 'email'       # Accesible mediante get_email_field_name()
-    REQUIRED_FIELDS = ['username']     # password y USERNAME_FIELD simpre son requeridos
-                                    # https://docs.djangoproject.com/en/4.1/topics/auth/customizing/#django.contrib.auth.models.CustomUser.REQUIRED_FIELDS
+    USERNAME_FIELD = 'email'  # Especifica que identidfica un usuario
+    EMAIL_FIELD = 'email'  # Accesible mediante get_email_field_name()
+    REQUIRED_FIELDS = ['username']  # password y USERNAME_FIELD simpre son requeridos
+    # https://docs.djangoproject.com/en/4.1/topics/auth/customizing/#django.contrib.auth.models.CustomUser.REQUIRED_FIELDS
     objects = HackerNewsUserManager()
 
     # Quizas util?
     def get_absolute_url(self):
-        return "/users/%i/" % (self.id)
-
-    def __str__(self):  # toString() de python
-        return self.username
+        return "/users/%i/" % self.id
 
     def has_perm(self, perm, obj=None):
-        "Does the user have a specific permission?"
+        """Does the user have a specific permission?"""
         # Simplest possible answer: Yes, always
         return True
 
     def has_module_perms(self, app_label):
-        "Does the user have permissions to view the app `app_label`?"
+        """Does the user have permissions to view the app `app_label`?"""
         # Simplest possible answer: Yes, always
         return True
 
