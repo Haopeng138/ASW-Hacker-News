585b5500365e4b30985ff810eb13d4fb1a0be52b
haopeng.lin <haopeng.lin@est.fib.upc.edu>
2022-11-09 16:13:12 +0100

    Añadiendo logout

--- hackersNews/homepage/urls.py
+++ hackersNews/homepage/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from . import views
-
+from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:page>', views.index, name='more'),
     path('login/', views.submit,name="login"),
     path('login/submit/create', views.create),
     path('login/submit/login', views.login),
-    path('logout',views.logout,name='logout'),
+    path('logout', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    #path('logout',views.logout,name='logout'),
     path('testProfile/',views.testProfile,name='profile'),
     path('testPost/',views.testPost,name='Post'),
     path('testSubmitPost/',views.testSubmitPost,name='testSubmitPost'),
