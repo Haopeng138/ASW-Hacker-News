038354908e2f8a0ac103ef7023e3865f2dc1e7aa
haopeng.lin <haopeng.lin@est.fib.upc.edu>
2022-11-09 17:00:10 +0100

    Se puede modificar el about del perfil

--- hackersNews/accounts/admin.py
+++ hackersNews/accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from accounts.models import HNUser
 # Register your models here.
+
+class Useradmim (admin.ModelAdmin):
+    pass
+
+admin.site.register(HNUser,Useradmim)
