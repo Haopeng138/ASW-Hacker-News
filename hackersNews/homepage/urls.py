15698eb5cd00ab93dd7de5f2aca1e3d8d7fd3bd1
marc.duch <marc.duch@estudiantat.upc.edu>
2022-11-09 18:51:48 +0100

    Añadido urls de modulo /accounts

--- hackersNews/homepage/urls.py
+++ hackersNews/homepage/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('login/', views.submit),
-    path('login/submit/create', views.create),
-    path('login/submit/login', views.login),
-    path('account/', views.account, name='account')
+    # path('login/', views.submit),
+    # path('login/submit/create', views.create),
+    # path('login/submit/login', views.login),
+    path('account/', views.account, name='account') # homepage/account/
 ]
