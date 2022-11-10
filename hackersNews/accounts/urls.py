81bc8378b376db5a38a589ca6d65cd347525bb77
marc.duch <marc.duch@estudiantat.upc.edu>
2022-11-10 13:12:49 +0100

    Rename de root account->login

--- hackersNews/accounts/urls.py
+++ hackersNews/accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.account , name="account"),        # Log in / Sign up
+    path('', views.account , name="login"),        # Log in / Sign up
     path('logout/', views.logout , name="logout"),   # Log out
     path('reset_password/', views.account , name="reset_password"),  # Reset password
     path('modify_password/', views.account , name="modify_password"), # Reset password
