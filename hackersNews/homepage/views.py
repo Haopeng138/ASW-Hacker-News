2398a557208c6dae7dd65b4721ecfbd1fb7d6f9a
marc.duch <marc.duch@estudiantat.upc.edu>
2022-11-10 13:14:25 +0100

    Commentado views deprecadas

--- hackersNews/homepage/views.py
+++ hackersNews/homepage/views.py
@@ -42,10 +42,11 @@ def check_submission(title, url, text):
             return False
     else:
         return True
-
+"""
 def login(request):		 
    template = loader.get_template('registration/login.html')		    
    return HttpResponse(template.render(None,request))
+"""
 
 def submit(request):
     if request.method == 'POST':
@@ -75,7 +76,7 @@ def submit(request):
         return render(request, 'post/submit.html')
 
 
-
+"""
 @csrf_exempt
 def create(request):
   usr = request.POST['acct']
@@ -85,7 +86,7 @@ def create(request):
   output = "Create Usr: " + usr + " Pw: " + pwd
   print(output)
   return HttpResponse(testIndex(request))
-
+"""
 # def login(request):
 #   usr = request.POST['acct']
 #   pwd = request.POST['pw']
@@ -94,18 +95,18 @@ def create(request):
 #   print(output)
 #   return HttpResponse(output,request)
 
-def logout(request):
+"""def logout(request):
   
   return redirect('/')
+"""
 
-
-def testIndex(request):
+"""def testIndex(request):
   myusers = Login.objects.all().values()
   template = loader.get_template('testUsr.html')
   context = { 'myusers' : myusers, }
   return HttpResponse(template.render(context, request))
-
-
+"""
+"""
 def account(request):
   # return redirect('/account/',permanent=True)
   userModel = get_user_model()
@@ -142,8 +143,49 @@ def account(request):
   # print(email,password,username)
 
   return render(request,'testLogin.html')
+"""
+"""
+def account(request):
+    userModel = get_user_model()
+    succesful_login_url='/homepage'
+    tempalte_name = 'testLogin.html'
 
+    # print("Model: " + userModel.__str__(self=userModel))
 
+    if request.POST.get('submit')=='Sign Up':
+        print("Sign up")
+
+        username=request.POST.get('username')
+        email=request.POST.get('email')
+        password=request.POST.get('pw')
+
+        if userModel.objects.filter(email=email).exists():
+            messages.warning(request,'Email already exists')
+
+        else:
+            user = userModel.objects.create_user(email=email,password=password,username=username)
+            user.set_password(password)
+            user.save()
+            messages.success(request,'User has been registered successfully')      #Dispalys message that user has been registerd
+            HN_AccountAdapter.login(self=HN_AccountAdapter, request=request,user=user)
+            return redirect(succesful_login_url)
+
+    elif request.POST.get('submit')=='Log in':
+        print("Login")
+
+        email = request.POST['email']
+        password = request.POST['pw']
+        user = authenticate(request, email=email, password=password)
+
+        if user is not None:
+            HN_AccountAdapter.login(self=HN_AccountAdapter,request=request, user=user)
+            print("Autheticated user: " + user.get_username())
+            return redirect (succesful_login_url)
+        else:
+            messages.warning(request,'Invalid credentials')
+
+    return render(request,template_name=tempalte_name)
+"""
 # def testProfile(request):
 #   template = loader.get_template('user/profile.html')
 #   context = {
