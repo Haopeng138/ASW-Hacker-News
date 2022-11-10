2482f816c148ebeb16d90588c91ae486a4fa34ee
marc.duch <marc.duch@estudiantat.upc.edu>
2022-11-10 13:12:20 +0100

    commentado urls deprecadas

--- hackersNews/homepage/urls.py
+++ hackersNews/homepage/urls.py
@@ -2,19 +2,23 @@ from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
 from django.contrib.auth.decorators import login_required
+
 urlpatterns = [
+    # HOMEPAGE
     path('', views.hackernews, name='index'),
+    path('', views.index, name='index'),
     path('<int:page>', views.index, name='more'),
     path('submit/', views.submit,name="submit"),
-    path('logout', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    #path('logout',views.logout,name='logout'),
-    #path('testProfile/',views.testProfile,name='profile'),
+    # path('logout', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    # path('logout',views.logout,name='logout'),
+    # path('testProfile/',views.testProfile,name='profile'),
+    # PROFILE
     path('profile/<int:user_id>',views.profile,name='profile'),
     path('profile/<int:user_id>/update', login_required(views.update_profile), name='update_profile'),
-    path('testPost/',views.testPost,name='Post'),
-    #path('testSubmitPost/',views.testSubmitPost,name='testSubmitPost'),
-    path('testPostEdit',views.testPostEdit,name='testPostEdit'),
-    path('account/', views.account, name='account'),
+    # path('testPost/',views.testPost,name='Post'),
+    # path('testSubmitPost/',views.testSubmitPost,name='testSubmitPost'),
+    # path('testPostEdit',views.testPostEdit,name='testPostEdit'),
+    # path('account/', views.account, name='account'),
     path('new', views.new, name='new'),
     path('new/<int:page>', views.new, name='new_more'),
     path('hackernews', views.hackernews, name='hackernews'),
@@ -24,10 +28,8 @@ urlpatterns = [
     path('post/<int:post_id>/edit', login_required(views.post_edit), name='post_edit'),
     path('post/<int:post_id>/delete', login_required(views.post_delete), name='post_delete'),
     path('upvote-post', views.upvote_post, name='upvote_post'),
-    #path('upvote-comment', views.upvote_comment, name='upvote_comment'),
-    path('', views.index, name='index'),
+    # path('upvote-comment', views.upvote_comment, name='upvote_comment'),
     # path('login/', views.submit),
     # path('login/submit/create', views.create),
     # path('login/submit/login', views.login),
-    path('account/', views.account, name='account') # homepage/account/
 ]
