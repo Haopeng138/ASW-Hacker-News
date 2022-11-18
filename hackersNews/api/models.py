34fa67e7df5293c142b78ef3aa009985278559a3
marc.duch <marc.duch@estudiantat.upc.edu>
2022-11-18 19:07:35 +0100

    UserAPIKey creado (quizas innecesario?)

--- hackersNews/api/models.py
+++ hackersNews/api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from rest_framework_api_key.models import AbstractAPIKey
 
 # Create your models here.
+
+class UserAPIKey(AbstractAPIKey):
+    user = models.ForeignKey('accounts.HNUser',
+                             on_delete=models.CASCADE,
+                             related_name="api_keys",
+                             #unique=True,
+                             )
