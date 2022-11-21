cc1433c43990419a7c6ea21404700a4baee96e3d
marc.duch <marc.duch@estudiantat.upc.edu>
2022-11-21 14:31:05 +0100

    Reducido codigo duplicado

--- hackersNews/accounts/views.py
+++ hackersNews/accounts/views.py
@@ -28,25 +28,24 @@ def account(request):
 
         else:
             user = userModel.objects.create_user(email=email,password=password,username=username)
-            user.set_password(password)
-            user.save()
-            messages.success(request,'User has been registered successfully')      #Dispalys message that user has been registerd
-            HN_AccountAdapter.login(self=HN_AccountAdapter, request=request, user=user)
-            return redirect(succesful_login_url)
+
+            # user.set_password(password)
+            # user.save()
+
+            #messages.success(request,'User has been registered successfully')
+            #HN_AccountAdapter.login(self=HN_AccountAdapter, request=request, user=user)
+            #return redirect(succesful_login_url)
+
+            return _login(request=request, succes_url=succesful_login_url, user=user)
 
     elif request.POST.get('submit')=='Log in':
         print("Login")
 
         email = request.POST['email']
         password = request.POST['pw']
-        user = authenticate(request, email=email, password=password)
 
-        if user is not None:
-            print("Autheticated user: " + user.get_username())
-            HN_AccountAdapter.login(self=HN_AccountAdapter,request=request, user=user)
-            return redirect (succesful_login_url)
-        else:
-            messages.warning(request,'Invalid credentials')
+        return _login(request, email=email, password=password, succes_url=succesful_login_url)
+
 
     return render(request,template_name=tempalte_name)
 
@@ -56,3 +55,20 @@ def logout(request):
         return redirect('/homepage')
     return redirect('/account')
 
+def _authenticate(email, password, request=None):
+    user = authenticate(request=request, email=email, password=password)
+    if user is None:
+        messages.warning(request, 'User credential invalid')
+    return user
+
+def _login(request, succes_url, user=None, email=None, password=None):
+
+    if user is not None:
+        print("Logging in user: " + user.get_username())
+        HN_AccountAdapter.login(self=HN_AccountAdapter, request=request, user=user)
+        return redirect(succes_url)
+    elif email is not None and password is not None:
+        return _login(request=request, user=_authenticate(email=email, password=password), succes_url=succes_url)
+    else:
+        messages.warning(request, 'Invalid credentials')
+        return redirect('/account')
