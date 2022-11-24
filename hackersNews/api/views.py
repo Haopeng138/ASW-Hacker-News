db5f9f79d65931eaeb801a08d8eacb9c5f50bd26
marc.duch <marc.duch@estudiantat.upc.edu>
2022-11-24 18:36:21 +0100

    user view -> Muesta info del usuario con la APIKey adecuada
    users_list -> Muestra todos los usuarios

--- hackersNews/api/views.py
+++ hackersNews/api/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.parsers import JSONParser
+from rest_framework_api_key.permissions import HasAPIKey
+from .models import UserAPIKey
+from accounts.models import HNUser
+from.serializers import HNUserSerializer
 
 # Create your views here.
+@csrf_exempt
+#@api_view(['GET'])
+@permission_classes([HasAPIKey])
+def user(request, id):
+    key = request.META["HTTP_AUTHORIZATION"].split()[1]
+
+    if id is not None:
+        try:
+            user = HNUser.objects.get(pk=id)
+        except HNUser.DoesNotExsist:
+            return HttpResponse(satus=404)
+
+    else:
+        try:
+            user = HNUser.objects.get(pk=id)
+        except HNUser.DoesNotExsist:
+            return HttpResponse(satus=404)
+
+    if request.method == 'GET':
+        serializer = HNUserSerializer(user)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = HNUserSerializer(user, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    return HttpResponse(status=400)
+
+@permission_classes([HasAPIKey])
+def users_list(request):
+    if request.method == 'GET':
+        users = HNUser.objects.all()
+        serializer = HNUserSerializer(users, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
