56e7e2a5f35a55fb40f80c4318eaa6b1be6043e8
marc.duch <marc.duch@estudiantat.upc.edu>
2022-12-01 19:42:26 +0100

    Reordenado urls

--- hackersNews/api/urls.py
+++ hackersNews/api/urls.py
@@ -3,11 +3,15 @@ from . import views
 
 urlpatterns = [
     # USERS
-    path('users/', views.users_list),
-    path('users/<int:id>/', views.user),
+    path('users/', views.user_list.as_view()),
+    path('users/<int:id>/', views.user_detail.as_view()),
+    path('users/<int:id>/comments', views.user_comments.as_view()),
+    path('users/<int:id>/submission', views.user_submissions.as_view()),
     # SUBMISSIONS
-    path('submissions/<int:id>/', views.sub_comment_list),
+    path('submissions/', views.submission_list.as_view()),
     path('submissions/<int:id>/', views.get_submission),
+    path('submissions/<int:id>/comments', views.sub_comment_list),
+
     # UPVOTE
     path('upvote-post/<int:id>/', views.upvote_post, name='upvote_post'),
     path('upvote-comment/<int:id>/', views.upvote_comment, name='upvote_comment'),
