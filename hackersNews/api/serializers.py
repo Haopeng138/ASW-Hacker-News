5652568dc77bd2fb8175dc31a9de9f2abd6f08eb
marc.duch <marcduchb20@gmail.com>
2022-12-09 22:15:40 +0100

    Added DynamicModelSerializer

--- hackersNews/api/serializers.py
+++ hackersNews/api/serializers.py
@@ -2,17 +2,40 @@ from rest_framework import serializers
 from accounts.models import HNUser
 from homepage.models import *
 
+class DynamicFieldsModelSerializer(serializers.ModelSerializer):
+    """ Model Serializer with Dynamic Set of Fields 
+        ref : https://stackoverflow.com/questions/23643204/django-rest-framework-dynamically-return-subset-of-fields"""
 
-class HNUserSerializer(serializers.Serializer):
+    def __init__(self, *args, **kwargs):
+        fields = kwargs.pop('fields', None)
+        # Instantiate the superclass normally
+        super(DynamicFieldsModelSerializer, self).__init__(*args, **kwargs)
+        if fields:
+            print("Apllying fields")
+            # Drop any fields that are not specified in the `fields` argument.
+            allowed = set(fields)
+            print(allowed)
+            existing = set(self.fields.keys())
+            print(existing)
+            for field_name in existing - allowed:
+                self.fields.pop(field_name)
+
+class HNUserSerializer(DynamicFieldsModelSerializer):
     id = serializers.IntegerField(read_only=True)
+
     username = serializers.CharField(required=False, allow_blank=False, max_length=255)
     email = serializers.EmailField(read_only=True)
     karma = serializers.IntegerField(read_only=True)
+
     date_joined = serializers.DateField(read_only=True)
     about = serializers.CharField(allow_blank=True, required=False)
     #is_admin = serializers.BooleanField(read_only=True)
 
-    def create(self, validated_data):
+    class Meta:
+        model = HNUser
+        fields = ['id','username','email','karma', 'date_joined', 'about']
+
+    def create(**validated_data):
         return HNUser.objects.create_user(**validated_data)
 
     def update(self, instance, validated_data):
@@ -22,24 +45,10 @@ class HNUserSerializer(serializers.Serializer):
         instance.save()
         return instance
 
-    # No muestra email
-class UnverifiedUserSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = HNUser
-        fields = ['id','username', 'karma','about','date_joined']
-
-# Serializador Siplificado para User (solo muestra su id y username)
-class SimplifiedUserSerializer(serializers.ModelSerializer):
-    id = serializers.IntegerField(read_only=True)
-    class Meta:
-        model = HNUser
-        fields = ['id','username']
-
-class CommentSerializer(serializers.ModelSerializer):
+class CommentSerializer(DynamicFieldsModelSerializer):
     id = serializers.IntegerField(read_only=True)
     postID = serializers.PrimaryKeyRelatedField(read_only=True, source='post')
-    user = SimplifiedUserSerializer(read_only=True, source='user')
+    user = HNUserSerializer(read_only=True, source='user', fields=('id','username'))
     insert_date = serializers.DateTimeField(read_only=True)
     replyTo = serializers.PrimaryKeyRelatedField(read_only=True, source='reply')
 
@@ -54,27 +63,34 @@ class CommentSerializer(serializers.ModelSerializer):
 
 
 class SimplifiedPostSerializer(serializers.ModelSerializer):
-    author = SimplifiedUserSerializer(read_only=True, source='user')
+    author = HNUserSerializer(read_only=True, source='user')
     commentID = CommentSerializer(read_only=True, source='comment_set')
     class Meta:
         model = Post
         fields = ['id','author','commentID']
 
 class SimplifiedCommentSerializer(serializers.ModelSerializer):
-    author = SimplifiedUserSerializer(source ='user', read_only = True)
+    author = HNUserSerializer(source ='user', read_only = True)
     post = SimplifiedPostSerializer(source = 'post', read_only=True)
     class Meta:
         model = Comment
         fields = ['id', 'post', 'author']
 
-class PostSerializer(serializers.ModelSerializer):
+class PostSerializer(DynamicFieldsModelSerializer):
     id = serializers.IntegerField(read_only=True)
+    
+    title = serializers.CharField(required=True)
     url = serializers.CharField(required=False)
     site = serializers.CharField(required=False, read_only=True)
     text = serializers.CharField(required=False)
-    votes = serializers.IntegerField(read_only=True)
+    
+    # votes = serializers.IntegerField(read_only=True)
+    votes = serializers.IntegerField()
     insert_date = serializers.DateTimeField(read_only=True)
-    user = SimplifiedUserSerializer(read_only=True)
+    
+    user = HNUserSerializer(read_only=True)
+    #userID = serializers.PrimaryKeyRelatedField(write_only=True, queryset='id')
+
     numComments = serializers.IntegerField(read_only=True)
     commentIDs = serializers.PrimaryKeyRelatedField(many=True, read_only=True, source ='comment_set')
     
