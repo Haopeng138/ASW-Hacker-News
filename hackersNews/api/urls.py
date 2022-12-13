1c818319f1708968b104c91e0a57d77219075c3c
marc.duch <marcduchb20@gmail.com>
2022-12-13 12:29:44 +0100

    Deleted block comment

--- hackersNews/api/urls.py
+++ hackersNews/api/urls.py
@@ -18,10 +18,4 @@ urlpatterns = [
     # UPVOTE
     path('vote-post/<int:id>/', views.submission_vote.as_view()),
     path('vote-comment/<int:id>/', views.comment_vote.as_view()),
-    """
-    path('upvote-post/<int:id>/', views.upvote_post, name='upvote_post'),
-    path('upvote-comment/<int:id>/', views.upvote_comment, name='upvote_comment'),
-    path('unvote-post/<int:id>/', views.unvote_post, name='unvote_post'),
-    path('unvote-comment/<int:id>/', views.unvote_comment, name='unvote_comment'),
-    """
 ]
