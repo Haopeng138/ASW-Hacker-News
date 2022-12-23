2d19820c03720646c2dfac599c92bf6b305e1bb4
marc.duch <marcduchb20@gmail.com>
2022-12-23 06:53:37 +0100

    Cleanup

--- hackersNews/api/urls.py
+++ hackersNews/api/urls.py
@@ -7,15 +7,16 @@ urlpatterns = [
     path('users/<int:id>/', views.user_detail.as_view()),
     path('users/<int:id>/comments', views.user_comments.as_view()),
     path('users/<int:id>/submission', views.user_submissions.as_view()),
+    
     # SUBMISSIONS
     path('submissions/', views.submission_list.as_view()),
     path('submissions/<int:id>/', views.submission_detail.as_view()),
-    path('submissions/<int:id>/comments', views.submission_comments.as_view()),
-    # COMMENTS
-    path('comments/<int:id>/',views.comment_detail.as_view()),
-    #path('comment/<int:id>/', views.reply_comment),
-
-    # UPVOTE
     path('submissions/<int:id>/vote', views.submission_vote.as_view()),
+    path('submissions/<int:id>/comments', views.submission_comments.as_view()),
+
+    # COMMENTS
+    path('comments/', views.comment_list.as_view()),
+    path('comments/<int:id>/',views.comment_detail.as_view()),
     path('comments/<int:id>/vote', views.comment_vote.as_view()),
+
 ]
